0776f7beee6ac38b03dc134773bc9159484ff6f2
Tanya Kozlova <tanya120606@mail.ru>
2025-03-05 21:38:40 +0300

    finalv3

--- lab1/src/paint_console/models/drawable_figures.py
+++ lab1/src/paint_console/models/drawable_figures.py
@@ -1,6 +1,7 @@
+import math
+
 from ..interfaces import IDrawable
 from ..utils import EllipseMath, RectangleMath, TriangleMath
-from ..renderers import EllipseRenderer, RectangleRenderer, TriangleRenderer
 from abc import abstractmethod
 
 
@@ -40,11 +41,20 @@ class DrawableEllipse(EllipseMath, DrawableFigure):
         DrawableFigure.__init__(self, background)
         EllipseMath.__init__(self, vertical_radius, horizontal_radius)
         self.__background = background
-        self.__renderer = EllipseRenderer()
 
     def render(self) -> list[list[str]]:
         """Represent the ellipse as a list of lines."""
-        return self.__renderer.render(self, self.__background)
+        vr = math.ceil(self.vertical_radius)
+        hr = math.ceil(self.horizontal_radius)
+        image = [[''] * (2 * hr) for _ in range(2 * vr)]
+
+        for y in range(2 * vr):
+            for x in range(2 * hr):
+                dx = x - hr + 0.5
+                dy = y - vr + 0.5
+                if (dx ** 2) / (hr ** 2) + (dy ** 2) / (vr ** 2) <= 1:
+                    image[y][x] = self.background
+        return image
 
     @property
     def info(self) -> dict:
@@ -61,11 +71,10 @@ class DrawableRectangle(RectangleMath, DrawableFigure):
         DrawableFigure.__init__(self, background)
         RectangleMath.__init__(self, width, height)
         self.__background = background
-        self.__renderer = RectangleRenderer()
 
     def render(self) -> list[list[str]]:
         """Represent the rectangle as a list of lines."""
-        return self.__renderer.render(self, self.__background)
+        return [[self.__background] * int(self.width) for _ in range(int(self.height))]
 
     @property
     def info(self) -> dict:
@@ -83,11 +92,18 @@ class DrawableTriangle(TriangleMath, DrawableFigure):
         DrawableFigure.__init__(self, background)
         TriangleMath.__init__(self, vertices)
         self.__background = background
-        self.__renderer = TriangleRenderer()
 
     def render(self) -> list[list[str]]:
         """Represent the triangle as a list of lines."""
-        return self.__renderer.render(self, self.__background)
+        max_x = int(max(x for x, y in self.vertices))
+        max_y = int(max(y for x, y in self.vertices))
+        image = [[''] * (max_x + 1) for _ in range(max_y + 1)]
+
+        for y in range(max_y + 1):
+            for x in range(max_x + 1):
+                if DrawableTriangle._is_point_inside(self.area, self.vertices, x, y):
+                    image[y][x] = self.background
+        return image
 
     @property
     def info(self) -> dict:
@@ -96,3 +112,17 @@ class DrawableTriangle(TriangleMath, DrawableFigure):
             **super().info,
             **super(TriangleMath, self).info
         }
+
+    @staticmethod
+    def _is_point_inside(basic_area, vertices, x, y) -> bool:
+        """Check if areas of all triangles in sum gives basic area."""
+        triangles_area = []
+        for a, b in zip(vertices, vertices[1:] + vertices[:1]):
+            try:
+                triangles_area.append(TriangleMath((a, b, (x, y))).area)
+            except ValueError:
+                triangles_area.append(0)
+
+        if sum(triangles_area) - basic_area < 1:
+            return True
+        return False
