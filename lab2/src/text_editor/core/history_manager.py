6e7b41b472c499c8d9bd2a54703086329999d8a4
Tanya Kozlova <tanya120606@mail.ru>
2025-03-16 15:59:46 +0300

    History manager add

---
+++ lab2/src/text_editor/core/history_manager.py
@@ -0,0 +1,46 @@
+from ..interfaces.icommand import ICommand
+
+
+def singleton(class_):
+    instances = {}
+
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+
+    return getinstance
+
+
+@singleton
+class HistoryManager:
+    def __init__(self):
+        self.__undo_stack: list[ICommand] = []
+        self.__redo_stack: list[ICommand] = []
+
+    def add_command(self, command: ICommand):
+        """Add a command to the stack."""
+        self.__undo_stack.append(command)
+        self.__redo_stack.clear()
+
+    def undo(self):
+        """Undo the last command."""
+        if self.__undo_stack:
+            command = self.__undo_stack.pop()
+            command.undo()
+            self.__redo_stack.append(command)
+        else:
+            raise IndexError("Can't undo (no commands)")
+
+    def redo(self):
+        """Redo the last undo command."""
+        if self.__redo_stack:
+            command = self.__redo_stack.pop()
+            command.redo()
+            self.__undo_stack.append(command)
+        else:
+            raise IndexError("Can't redo (no commands)")
+
+    def clear(self):
+        self.__undo_stack.clear()
+        self.__redo_stack.clear()
