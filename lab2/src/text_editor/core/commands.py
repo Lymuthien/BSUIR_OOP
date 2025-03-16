26bc88f20b603c20f71da23b2031599d7c200e42
Tanya Kozlova <tanya120606@mail.ru>
2025-03-16 15:59:24 +0300

    Commands interface and future classes.

---
+++ lab2/src/text_editor/core/commands.py
@@ -0,0 +1,13 @@
+from ..interfaces.icommand import ICommand
+
+class WriteCommand(ICommand):
+    pass
+
+class EraseCommand(ICommand):
+    pass
+
+class PasteCommand(ICommand):
+    pass
+
+class ChangeStyle(ICommand):
+    pass
