fb5143314c7d115c0a9b16d9d23ab135b5537f49
Tanya Kozlova <tanya120606@mail.ru>
2025-05-10 18:02:23 +0300

    Add repository

--- lab3/domain/student.py
+++ lab3/domain/student.py
@@ -7,9 +7,25 @@ class IStudent(Entity):
 
 class Student(IStudent):
     def __init__(self, id_, name: str, grade: int):
+        self.validate_name(name)
+        self.validate_grade(grade)
         self.name = name
         self.grade = grade
         super().__init__(id_)
+
+    @staticmethod
+    def validate_name(name: str):
+        if not isinstance(name, str):
+            raise ValueError("Student name must be a string")
+        if not name:
+            raise ValueError("Student name cannot be empty")
+
+    @staticmethod
+    def validate_grade(grade: int):
+        if not isinstance(grade, int):
+            raise ValueError("Student grade must be a integer")
+        if grade < 0 or grade > 10:
+            raise ValueError("Student grade must be between 0 and 10")
 
     def to_dict(self):
         return {"id": self.id, "name": self.name, "grade": self.grade}
---
+++ lab3/persistence/student_repository.py
@@ -0,0 +1,57 @@
+import copy
+import json
+import os
+from ..domain import Student, IRepository
+
+
+class StudentRepository(IRepository):
+    def __init__(self, file_path="students.json"):
+        self.file_path = file_path
+        self.students: list[Student] = []
+        self.next_id = 1
+        self.load()
+
+    def get_all(self) -> list[Student]:
+        return self.students.copy()
+
+    def get_by_id(self, id_: int) -> Student | None:
+        for student in self.students:
+            if student.id == id_:
+                return copy.deepcopy(student)
+        return None
+
+    def load(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r") as f:
+                data = json.load(f)
+                self.students = [Student.from_dict(item) for item in data]
+                if self.students:
+                    self.next_id = max(student.id for student in self.students) + 1
+        else:
+            self.students = []
+            self.next_id = 1
+
+    def save(self):
+        with open(self.file_path, "w") as f:
+            data = json.dumps([student.to_dict() for student in self.students])
+            f.write(data)
+
+    def add(self, student: Student):
+        student.id = self.next_id
+        self.students.append(student)
+        self.next_id += 1
+        self.save()
+
+    def update(self, student: Student):
+        for u_student in self.students:
+            if u_student.id == student.id:
+                u_student.name = student.name
+                u_student.grade = student.grade
+                self.save()
+                return True
+        return False
+
+    def delete(self, student: Student):
+        for u_student in self.students:
+            if u_student.id == student.id:
+                self.students.remove(u_student)
